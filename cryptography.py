29386c0a2a976886404e901e35f7a35af2999099
Amundeep Singh Dhaliwal <64365442+Amundeep-Dhaliwal@users.noreply.github.com>
2020-09-06 10:33:38 +0100

    Update cryptography.py

--- cryptography.py
+++ cryptography.py
@@ -16,3 +16,41 @@ def caesarCipher(text, offset):
                 universal = ((value + offset)%26)+97
                 legible += chr(universal)
     return legible
+
+# A Vigenere cipher implementation to both encode and decode strings given a key
+from itertools import cycle
+import string
+
+def caesarPortion(letter, offset):
+    if letter.isupper():
+        value = ord(letterCode) -65
+        universal = ((value + offset) %26)+65
+        return chr(universal)
+    else:
+        value = ord(letter) - 97
+        universal = ((value + offset)%26)+97
+        return chr(universal)
+
+def vigenereCipher(message, key, decode = False):
+    key = key.lower()
+    returnString = ''
+    looper = cycle(range(len(key)))
+    uppercase = {x for x in range(65,91)}
+    lowercase = {x for x in range(97, 123)}
+    offsets = dict(zip(string.ascii_lowercase, range(26)))
+    if decode:
+        coefficent = -1
+    else:
+        coefficent = 1
+    for letter in message:
+        if ord(letter) in uppercase:
+            keyIndex = next(looper)
+            offset = offsets[key[keyIndex]]
+            returnString += caesarPortion(letter, coefficent * offset)
+        elif ord(letter) in lowercase:
+            keyIndex = next(looper)
+            offset = offsets[key[keyIndex]]
+            returnString += caesarPortion(letter, coefficent * offset)
+        else:
+            returnString += letter
+    return returnString
